d92a7655d29c6684c22c47b30adb983e7079a07b
Isoruku <DEVKUMARMONDAL.25101040289@gehu.ac.in>
2026-09-08 17:09:21 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './*.{js,jsx}', './src/**/*.{js,jsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
